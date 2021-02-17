673dc8ec91e32fbe1f6de20e621e14fae5827186
Alberto <albertofagner.cav@gmail.com>
2021-02-17 13:45:57 -0300

    added saturation increase

--- saturation/__init__.py
+++ saturation/__init__.py
@@ -1,6 +1,13 @@
 import cv2
+import numpy as np
 
 
 def saturation(frame):
-    
-    return frame
+    imghsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV).astype("float32")
+    (h, s, v) = cv2.split(imghsv)
+    s = s * 1.5
+    s = np.clip(s, 0, 255)
+    imghsv = cv2.merge([h,s,v])
+    imgbgr = cv2.cvtColor(imghsv.astype("uint8"), cv2.COLOR_HSV2BGR)
+
+    return imgbgr
